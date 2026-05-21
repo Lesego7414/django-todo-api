a8b56cb89fde2a858be03d773e9564376d608bf8
Lesego7414 <lesego.poisonpen@gmail.com>
2026-05-21 23:28:56 +0200

    Fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4j^_3i9wg1!m(vepm0m#(87awe)71_5s-aqo(pu3v13gp=xagd
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "django-todo-api-xi64.onrender.com",
+    "django-todo-api-1-4sbv.onrender.com",
     "127.0.0.1",
     "localhost"
 ]
